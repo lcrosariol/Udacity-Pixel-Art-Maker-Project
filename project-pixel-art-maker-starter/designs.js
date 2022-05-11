241c19458a9f0be89d277c75cf48e7be7760a4a8
lauren <laurencrosariol@yahoo.com>
2022-05-10 21:57:58 -0700

    updated solution

--- project-pixel-art-maker-starter/designs.js
+++ project-pixel-art-maker-starter/designs.js
@@ -1,43 +1,35 @@
 // Select color input
 // Select size input
 var height, width, color;
+// const colorPicker = document.getElementById("colorPicker");
+const pixelCanvas = document.getElementById("pixelCanvas");
 
-// whtn the user selects a size, call makeGrid()
-$('#sizePicker').submit(function (event) {
-    //used on a submit button to prevent it from submitting a form
+// add event on click (submit button)
+document.getElementById("sizePicker").addEventListener("submit", function(event){
+    //prevents page refresh
     event.preventDefault();
-    //get the values while the button is clicked 
-    height = $('#inputHeight').val();
-    width = $('#inputWidth').val();
-    //function makeGrid takes parameters of height and width
-    makeGrid(height, width);
-})
+    //gets height value
+    const inputHeight = document.getElementById("inputHeight").value;
+    //gets width value
+    const inputWidth = document.getElementById("inputWidth").value;
+    //calls makeGrid function to make a grid with the input values
+    makeGrid(inputHeight, inputWidth);
+});
 
-function makeGrid(x, y) {
+
+function makeGrid(inputHeight, inputWidth) {
     //remove any existing table rows, which will remove the table data's as well
-    $('tr').remove();
-    //loop over the height
-    for (var i = 1; i <= x; i++) {
-        //create a table row with an id of pixelCanvas for height selected by user
-        $('#pixelCanvas').append('<tr id=table' + i + '></tr>');
-        //loop over the width submission
-        for (var j = 1; j <= y; j++) {
-            //add a table data column to the row
-            $('#table' + i).append('<td></td>');
-        }
-    }
-    //when user clicks on the cell add color
-    $('td').click(function addColor() {
-        //get the value of the id of colorPicker and set it to our variable
-        color = $('#colorPicker').val();
-        // if this cell has a style attribute, 
-        if ($(this).attr('style')) {
-            //remove the styling
-            $(this).removeAttr('style')
-            //if not..
-        } else {
-            //style the cell with the background of the color selected
-            $(this).attr('style', 'background-color:' + color);
-        }
-    })
-}
+    pixelCanvas.innerHTML= "";
+    let colorPicker = document.getElementById("colorPicker");
+    // iterate over the rows and columns and add them to the grid
+    for (var i = 0; i < inputHeight; i++) {
+        var row = pixelCanvas.insertRow(i);
+            for (var j = 0; j < inputWidth; j++){
+                let cell = row.insertCell(j);
+                //add selected color to the cell
+                cell.addEventListener("click", function(event) {
+                    event.target.style.backgroundColor = colorPicker.value;
+                });
+            };
+    };
+};
