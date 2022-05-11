4399d4af4fff4eb9ee3395e8f641c352345d7c26
lauren <laurencrosariol@yahoo.com>
2022-05-11 01:07:26 -0700

    fixed error i accidentallly added

--- project-pixel-art-maker-starter/designs.js
+++ project-pixel-art-maker-starter/designs.js
@@ -3,23 +3,23 @@ document.getElementById("sizePicker").addEventListener("submit", function(event)
     //prevents page refresh
     event.preventDefault();
     //gets height value
-    var inputHeight = document.getElementById("inputHeight").value;
+    const inputHeight = document.getElementById("inputHeight").value;
     //gets width value
-    var inputWidth = document.getElementById("inputWidth").value;
+    const inputWidth = document.getElementById("inputWidth").value;
     //makes the grid, passes selected height and width needed
     makeGrid(inputHeight, inputWidth);
 });
 
-
+//makeGrid function makes the grid
 function makeGrid(inputHeight, inputWidth) {
     //remove any existing table rows, which will remove the table data's as well
     pixelCanvas.innerHTML= "";
-    let colorPicker = document.getElementById("colorPicker");
+    const colorPicker = document.getElementById("colorPicker");
     // iterate over the rows and columns and add them to the grid
-    for (let i = 0; i < inputHeight; i++) {
+    for (var i = 0; i < inputHeight; i++) {
         //insert rows
-        let row = pixelCanvas.insertRow(i);
-            for (let j = 0; j < inputWidth; j++){
+        var row = pixelCanvas.insertRow(i);
+            for (var j = 0; j < inputWidth; j++){
                 //insert cells
                 let cell = row.insertCell(j);
                 //add selected color to the cell
@@ -29,3 +29,4 @@ function makeGrid(inputHeight, inputWidth) {
             };
     };
 };
+
